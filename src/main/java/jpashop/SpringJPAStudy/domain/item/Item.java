225ae5217a806467052461284112d6170dcdf6d9
redHeadSet <stikfascube@gmail.com>
2021-11-01 16:20:18 +0900

    Item 관련 비즈니스 로직 및 Repository 추가

--- src/main/java/jpashop/SpringJPAStudy/domain/item/Item.java
+++ src/main/java/jpashop/SpringJPAStudy/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpashop.SpringJPAStudy.domain.item;
 
 
 import jpashop.SpringJPAStudy.domain.Category;
+import jpashop.SpringJPAStudy.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,16 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items", fetch = FetchType.LAZY)
     private List<Category> categories = new ArrayList<>();
+    
+    // 비즈니스 로직 추가
+    public void addStock(int stockQuantity){
+        this.stockQuantity += stockQuantity;
+    }
+
+    public void romoveStock(int removeQuantity){
+        int restStock = this.stockQuantity - removeQuantity;
+        if(restStock < 0)
+            throw new NotEnoughStockException("need more stock");
+        this.stockQuantity = restStock;
+    }
 }
