38dc172779a0ce8497a89edd7b47756b2319a930
redHeadSet <stikfascube@gmail.com>
2021-11-01 16:26:08 +0900

    Item 관련 서비스 작성

---
+++ src/main/java/jpashop/SpringJPAStudy/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpashop.SpringJPAStudy.service;
+
+import jpashop.SpringJPAStudy.domain.item.Item;
+import jpashop.SpringJPAStudy.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    final private ItemRepository itemRepository;
+
+    @Transactional(readOnly = false)
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public Item findItem(Long id) {
+        return itemRepository.findOne(id);
+    }
+
+    public List<Item> findAll(){
+        return itemRepository.findAll();
+    }
+}
