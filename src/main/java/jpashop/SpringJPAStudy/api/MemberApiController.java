c158de26e360609e77136c5c94a42c410ce97d92
redHeadSet <stikfascube@gmail.com>
2021-11-11 16:17:22 +0900

    멤버 등록 api 생성 : DTO 처리

---
+++ src/main/java/jpashop/SpringJPAStudy/api/MemberApiController.java
@@ -0,0 +1,57 @@
+package jpashop.SpringJPAStudy.api;
+
+import jpashop.SpringJPAStudy.domain.Member;
+import jpashop.SpringJPAStudy.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+// @Controller
+// @ResponseBody // json, xml 등으로 통신할 때 사용하는 어노테이션
+@RestController // 위의 2개의 어노테이션을 합친 것
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMember(@RequestBody // json 데이터를 Member로 바꾸는 역할
+                                           @Valid Member member) {
+        // 엔티티를 지금과 같이 직접 처리하는 것은 문제가 발생할 여지가 있음 - 엔티티를 외부에 노출
+        // api 수정 or Member class 수정 시 api 스펙 자체가 바뀌게 되면서 여러 사이드이펙트 생길 수 있음
+        // 즉, DTO 필요
+        Long joinId = memberService.join(member);
+        return new CreateMemberResponse(joinId);
+    }
+
+    // 별도의 DTO 사용
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMember(@RequestBody @Valid CreateMemberRequest memberRequest){
+        Member member = new Member();
+        member.setName(memberRequest.getName());
+
+        memberService.join(member);
+
+        return new CreateMemberResponse(member.getId());
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+
+    }
+}
